dd0e0236bde0a2b6f98fa5f3fd281c761e61c873
edgar-tavares <edgarbunito123@gmail.com>
2025-02-16 22:27:26 -0300

    first commit

---
+++ src/test/java/com/Aos/marcio/Hello/Word/HelloWordApplicationTests.java
@@ -0,0 +1,14 @@
+package com.Aos.marcio.Hello.Word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Hello Word");
+	}
+
+}
